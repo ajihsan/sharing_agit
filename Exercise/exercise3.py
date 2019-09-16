094066794b88bf0adb1c10a640e5d68e844f7d97
Muhammad Ihsan <ajihsan96@gmail.com>
2019-09-16 16:29:53 +0700

    Create exercise3.py

---
+++ Exercise/exercise3.py
@@ -0,0 +1,8 @@
+"""
+Dari 2 array, buat array yang berisi nama yang muncul di salah satu atau kedua array-nya. Hasil array tidak boleh memiliki duplikat.
+Hint: gunakan loop dan condition
+"""
+
+names1 = ["Ava", "Emma", "Olivia"]
+names2 = ["Olivia", "Sophia", "Emma"]
+names_result = []
