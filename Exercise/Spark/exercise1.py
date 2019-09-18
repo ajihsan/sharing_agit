e2be218ae09061050c6fc6b38b274051f05b90cf
Muhammad Ihsan <ajihsan96@gmail.com>
2019-09-18 14:24:59 +0700

    change sikit

---
+++ Exercise/Spark/exercise1.py
@@ -0,0 +1,22 @@
+"""
+Case: PT. Pos Indonesia
+
+'Perusahaan Pos Indonesia ingin mengetahui reaksi dan komentar customer terhadap perusahaan mereka di sosial media'
+
+Load dataframe dari 3 data yang berada di folder exercise:
+1. AkunTwitter_POS.csv
+2. HashtagTwitter_POS.csv
+3. Instagram_POS.json
+
+Check schema dari masing-masing dataframe. Data tersebut berasal dari 2 sosial media yang berbeda: twitter & instagram. 
+
+Gabungan 3 dataframe diatas agar menjadi 1 format. Menjadi format:
+
+| Username   | Content       | Source    |
+| duldul     | kok surat..   | Twitter   |
+| fathur12   | Halo,tolong.. | Instagram |
+
+Hint: 
+1. sesuaikan 'content' agar merepresentasikan aspirasi customer Pos Indonesia di masing-masing sosial media
+2. Pastikan hanya data customer yang digabungkan
+"""
